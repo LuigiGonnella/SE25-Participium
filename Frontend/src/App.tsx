0ac0b1b1cabbf26cb5f205aeeb8bf8235b73c389
EAnzaldi <elisabettaanzaldi1@gmail.com>
2025-11-04 18:33:05 +0100

    Added Design React Kit from Bootstrap Italia

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,10 @@ import './App.css'
 import HomePage from './components/Home';
 import LoginForm from './components/LoginPage';
 import DefaultLayout from './components/DefaultLayout';
+import 'bootstrap-italia/dist/css/bootstrap-italia.min.css';
+import 'typeface-titillium-web';
+import 'typeface-roboto-mono';
+import 'typeface-lora';
 
 function App() {
   return (
