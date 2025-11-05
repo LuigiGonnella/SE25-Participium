0bd04240e93a7b431e028d55f6bf8df0deab6782
EAnzaldi <elisabettaanzaldi1@gmail.com>
2025-11-05 13:48:48 +0100

    Added password in registration form

--- Frontend/src/components/RegistrationPage.tsx
+++ Frontend/src/components/RegistrationPage.tsx
@@ -1,4 +1,3 @@
-import { Label } from 'design-react-kit';
 import { useState } from 'react';
 import {Form, FormGroup, Row, Col, Button, Alert} from 'react-bootstrap';
 
@@ -8,6 +7,8 @@ function RegistrationForm(){
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [receiveEmails, setReceiveEmails] = useState(false);
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     return (
     <>
@@ -18,105 +19,137 @@ function RegistrationForm(){
                 alignItems: "center",
                 height: "100vh",
             }}>
-      <Form >
-        <h2 className="text-center mb-3">Registration</h2>
-        <Row>
-            <Col>
-          <Form.Group className="mb-3" controlId="formName">
-            <Form.Label>Name</Form.Label>
-            <Form.Control
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                isInvalid={name === ''}
-              />
-              <Form.Control.Feedback type="invalid">
-                This field is required
-              </Form.Control.Feedback>
-          </Form.Group>
-          </Col>
-          </Row>
+        <Form >
+          <h2 className="text-center mb-3">Registration</h2>
           <Row>
-            <Col>
-          <FormGroup className="mb-3" controlId="formSurname">
-            <Form.Label>Surname</Form.Label>
-            <Form.Control
-                type="text"
-                value={surname}
-                onChange={(e) => setSurname(e.target.value)}
-                isInvalid={surname === ''}
-              />
-              <Form.Control.Feedback type="invalid">
-                This field is required
-              </Form.Control.Feedback>
-          </FormGroup>
-          </Col>
-          </Row>
-          <Row>
-            <Col>
-          <FormGroup className="mb-3" controlId ="formUsername">
-            <Form.Label>Username</Form.Label>
-            <Form.Control
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                isInvalid={username === ''}
-              />
-              <Form.Control.Feedback type="invalid">
-                This field is required
-              </Form.Control.Feedback>
-          </FormGroup>
-          </Col>
-          </Row>
-          <Row>
-            <Col>
-          <FormGroup className="mb-3" controlId="formEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                isInvalid={email === ''}
-              />
-              <Form.Control.Feedback type="invalid">
-                This field is required
-              </Form.Control.Feedback>
-          </FormGroup>
-          </Col>
-          </Row>
-          <Row>
-            <Col>
-            <Form.Group className="mb-3">
-                <Form.Check
-                type="checkbox"
-                id="terms"
-                label="Receive notifications via email"
-                checked={receiveEmails}
-                onChange={(e) => setReceiveEmails(e.target.checked)}
-                />
+              <Col>
+                <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                      type="text"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      isInvalid={name === ''}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      This field is required.
+                    </Form.Control.Feedback>
                 </Form.Group>
-            </Col>
+              </Col>
             </Row>
-        <Row className='align-items-center'>
-          <Col
-            md='3'
-            lg='6'
-            className='mt-3 mt-md-0 d-flex justify-content-center justify-content-md-end justify-content-lg-start'
-          >
-            <Button color='primary' type='submit'>
-              Invia
-            </Button>
+            <Row>
+              <Col>
+                <FormGroup className="mb-3" controlId="formSurname">
+                  <Form.Label>Surname</Form.Label>
+                  <Form.Control
+                      type="text"
+                      value={surname}
+                      onChange={(e) => setSurname(e.target.value)}
+                      isInvalid={surname === ''}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      This field is required.
+                    </Form.Control.Feedback>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <FormGroup className="mb-3" controlId ="formUsername">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                      type="text"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      isInvalid={username === ''}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      This field is required.
+                    </Form.Control.Feedback>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <FormGroup className="mb-3" controlId="formEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      isInvalid={email === ''}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      This field is required.
+                    </Form.Control.Feedback>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <FormGroup className="mb-3" controlId="formPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      isInvalid={password === '' || password.length < 8}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Your password must contain at least 8 characters.
+                    </Form.Control.Feedback>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group className="mb-3" controlId="formConfirmPassword">
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Form.Control
+                      type="password"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      isInvalid={confirmPassword === '' && confirmPassword !== password}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Password does not match.
+                    </Form.Control.Feedback>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Group className="mb-3">
+                    <Form.Check
+                    type="checkbox"
+                    id="terms"
+                    label="Receive notifications via email"
+                    checked={receiveEmails}
+                    onChange={(e) => setReceiveEmails(e.target.checked)}
+                    />
+                </Form.Group>
+              </Col>
+              </Row>
+          <Row className='align-items-center'>
+            <Col
+              md='3'
+              lg='6'
+              className='mt-3 mt-md-0 d-flex justify-content-center justify-content-md-end justify-content-lg-start'
+            >
+              <Button color='primary' type='submit'>
+                Invia
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+        <Row className='mt-4'>
+          <Col>
+            <Alert variant='danger'>
+              <strong>Attention</strong> Some fields need to be checked.
+            </Alert>
           </Col>
         </Row>
-      </Form>
-      <Row className='mt-4'>
-        <Col>
-          <Alert variant='danger'>
-            <strong>Attention</strong> Some fields need to be checked.
-          </Alert>
-        </Col>
-      </Row>
-                </div>
+    </div>
     </>
   );
 }
